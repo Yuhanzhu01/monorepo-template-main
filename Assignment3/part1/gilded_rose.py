0172a32026d341ffd188153f88bdb791c2a88c69
Yuhanzhu01 <zhuyuhan.2018@gmail.com>
2023-10-04 21:37:37 -0700

    submit A3

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -2,8 +2,8 @@
 
 
 class Item:
-    """ DO NOT CHANGE THIS CLASS!!!"""
     def __init__(self, name, sell_in, quality):
+        """ DO NOT CHANGE THIS CLASS!!!"""
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
@@ -12,38 +12,74 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class DefaultInventoryUpdate:
+    @staticmethod
+    def update(item):
+        if item.sell_in >= 0:
+            item.sell_in -= 1
+            item.quality -= 1
+        else:
+            item.sell_in -= 1
+            item.quality -= 2
+        item.quality = max(0, item.quality)
 
-    def __init__(self, items: list[Item]):
-        # DO NOT CHANGE THIS ATTRIBUTE!!!
+
+class AgedBrieUpdate:
+    @staticmethod
+    def update(item):
+        item.sell_in -= 1
+        item.quality += 1
+        item.quality = min(50, item.quality)
+
+
+class SulfurasUpdate:
+    @staticmethod
+    def update(item):
+        # Sulfuras stays the same
+        pass
+
+
+class BackstagePassesUpdate:
+    @staticmethod
+    def update(item):
+        if item.sell_in > 10:
+            item.quality += 1
+        elif 5 < item.sell_in <= 10:
+            item.quality += 2
+        elif 0 < item.sell_in <= 5:
+            item.quality += 3
+        elif item.sell_in <= 0:
+            item.quality = 0
+
+        item.sell_in -= 1
+        item.quality = min(50, item.quality)
+
+
+class ConjuredUpdate:
+    @staticmethod
+    def update(item):
+        if item.sell_in >= 0:
+            item.sell_in -= 1
+            item.quality -= 2
+        else:
+            item.sell_in -= 1
+            item.quality -= 4
+        item.quality = max(0, item.quality)
+
+
+class GildedRose(object):
+    def __init__(self, items):
         self.items = items
+        self.update_allItems = {
+            "Aged Brie": AgedBrieUpdate,
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdate,
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassesUpdate,
+            "Conjured Mana Cake": ConjuredUpdate,
+        }
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            strategy = self.update_allItems.get(item.name, DefaultInventoryUpdate)
+            strategy.update(item)
+
+
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -4,12 +4,52 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+class TestGildedRose(unittest.TestCase):
+    def test_default_inventory_update(self):
+        item = Item("Default Item", 5, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 9)
+
+    def test_aged_brie_update(self):
+        item = Item("Aged Brie", 5, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 11)
+
+    def test_sulfuras_update(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 5, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 5)
+        self.assertEqual(item.quality, 10)
+
+    def test_backstage_passes_update(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 11, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 10)
+        self.assertEqual(item.quality, 11)
+
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 13)
+
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 0)
+
+    def test_conjured_update(self):
+        item = Item("Conjured Mana Cake", 5, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 8)
+
+    def test_unknown_item_update(self):
+        item = Item("Unknown Item", 5, 10)
+        GildedRose([item]).update_quality()
+        self.assertEqual(item.sell_in, 4)
+        self.assertEqual(item.quality, 9)
 
 
 if __name__ == '__main__':
